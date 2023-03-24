05d7c9676dc3f68fdb74948400b1c6fef558d195
Bobby <31149862+klson@users.noreply.github.com>
2023-03-24 02:57:22 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,26 +1,11 @@
 using System;
 
-// 2023-03-23 18:21:33.546457700 UTC
-
-namespace hazedumper
+namespace ApprovedHWIDS
 {
-    public const Int32 timestamp = 1679595693;
-    public static class netvars
+    public const Int32 timestamp = 0;
+    public static class Approvals
     {
-        public const Int32 B01 = 0x0;
-        public const Int32 BDHID = 0x2720196500;
-        public const Int32 BN01 = 0x426F626279;
+        public const Int32 HID0 = 0x2720196500;
+        public const Int32 NME0 = 0xBOBBY;
     }
 }
-aaac B01 = 0x0;
-aaad BDHID = 0x2720196500;
-aaae BN01 = 0xBobby;
-
-aaac B02 = AX1;
-aaad BD02 = AX1579337184;
-aaae BN02 = AXJames;
-
-aaac B03 = AX2;
-aaad BD03 = AX23393905;
-aaae BN03 = AXEddie;
-
