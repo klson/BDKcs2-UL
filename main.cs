1b2dbedbf0f41771fbbda350ce6a0a9c0d4c6222
Bobby <31149862+klson@users.noreply.github.com>
2023-03-24 02:51:30 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,3 +1,17 @@
+using System;
+
+// 2023-03-23 18:21:33.546457700 UTC
+
+namespace hazedumper
+{
+    public const Int32 timestamp = 1679595693;
+    public static class netvars
+    {
+        public const Int32 cs_gamerules_data = 0x0;
+        public const Int32 m_ArmorValue = 0x117CC;
+        public const Int32 m_Collision = 0x320;
+        public const Int32 m_CollisionGroup = 0x474;
+
 aaac B01 = 0x0;
 aaad BDHID = 0x2720196500;
 aaae BN01 = 0xBobby;
