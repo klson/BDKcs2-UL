e931324cd82e5af9acbbd5bbacf72922ab95b91c
Bobby <31149862+klson@users.noreply.github.com>
2023-03-24 02:47:55 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,12 +1,12 @@
-aaac B01 = 0x0;
-aaad BD01 = 2720196500;
-aaae BN01 = 0xBobby;
+aaac B01 = AX0;
+aaad BD01 = AX2720196500;
+aaae BN01 = AXBobby;
 
-aaac B02 = 0x1;
-aaad BD02 = 1579337184;
-aaae BN02 = 0xJames;
+aaac B02 = AX1;
+aaad BD02 = AX1579337184;
+aaae BN02 = AXJames;
 
-aaac B03 = 0x2;
-aaad BD03 = 1023393905;
-aaae BN03 = 0xEddie;
+aaac B03 = AX2;
+aaad BD03 = AX23393905;
+aaae BN03 = AXEddie;
 
