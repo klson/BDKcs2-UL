1762407f6e366616f96270a8d8ae6d8eb3fb2a3a
Bobby <31149862+klson@users.noreply.github.com>
2023-10-17 13:01:43 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,7 +6,5 @@ namespace ApprovedHWIDS
     public static class Approvals
     {
         public const Int32 HID0 = 0xA222E794; //BOBBY
-        public const Int32 HID1 = 0x5E22C5E0; //JAMES
-        public const Int32 HID2 = 0x3CFFC071; //EDDIE
     }
 }
