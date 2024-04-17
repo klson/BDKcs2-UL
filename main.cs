116f29dd53a2dad5168b8aff8be1eeefb63a102b
BobbyY <31149862+klson@users.noreply.github.com>
2024-04-17 21:43:00 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,6 +8,7 @@ namespace ApprovedHWIDS
         public const Int32 HID0 = 0xA222E794; //BOBBY
         public const Int32 HID1 = 0x5E22C5E0; //JAMES
         public const Int32 HID2 = 0x3CFFC071; //EDDIE
-        public const Int32 HID3 = 0x60385528; //JOHNNY
+        public const Int32 HID3 = 0xB0E960A0; //EDDIE
+        public const Int32 HID4 = 0x60385528; //JOHNNY
     }
 }
